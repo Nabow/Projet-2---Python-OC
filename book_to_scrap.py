f5eb6463ad702347b7807e6bf3733c9b2e9cbccd
Raphael Huet de Froberville <rdefrob@gmail.com>
2021-09-20 14:36:21 +0200

    en dours de débogage get values

--- book_to_scrap.py
+++ book_to_scrap.py
@@ -1,14 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import csv
-
-
-# récupère les titres ou descriptions comme liste de strings
-def extraire_donnees(elements):
-	resultat = []
-	for element in elements:
-		resultat.append(element.string)
-	return resultat
+import nums_from_string
 
 
 def get_in_soup(url):
@@ -18,6 +11,45 @@ def get_in_soup(url):
 
 	# transforme (parse) le HTML en objet BeautifulSoup
 	return BeautifulSoup(page, "html.parser")
+
+
+# récupère les titres ou descriptions comme liste de strings
+def extraire_donnees(elements):
+	universal_product_code = []
+    title_product = []
+    price_including_tax = []
+    price_excluding_tax = []
+    number_available = []
+    product_description = []
+    category = []
+    review_rating = []
+    image_url = []
+    results = []
+
+	for element in elements:
+
+        soup = get_in_soup(element)
+
+        universal_product_code.append(soup.find("th" , text="UPC" ).next_sibling)
+        title_product.append(soup.h1.string)
+        price_including_tax.append(soup.find("th" , text="Price (incl. tax)" ).next_sibling)
+        price_excluding_tax.append(soup.find("th" , text="Price (excl. tax)" ).next_sibling)
+        
+        availability = soup.find("p" , class_="instock availability").text
+        number_available.append(nums_from_string.get_nums(availability))
+
+        product_description.append(soup.find( id="product_description" ).next_sibling.next_sibling)  #Ne marche pas
+
+        test = soup.find("ul" , class_="breadcrumb" )
+        cat = test.select("li:nth-child(3) > a") #Ne marche pas
+        category.append(cat.string)
+
+
+        review_rating_score = soup.find("p" , class_="star-rating *")
+    
+        image_url
+
+    return results = [elements, universal_product_code, title_product, price_including_tax, price_excluding_tax, number_available, product_description, category, review_rating, image_url ]
 
 
 # charger la donnée dans un fichier csv
@@ -37,7 +69,7 @@ def scroll_page():
 def get_all_urls(url):
     page = url + "page-1.html"
 
-    table_urls = []
+    product_page_url = []
 
     while (page):
         
@@ -45,21 +77,21 @@ def get_all_urls(url):
 
         products = soup.find_all("h3")  
         for product in products:
-            table_urls.append( url + product.find("a").get('href') )
+            product_page_url.append( url + product.find("a").get('href') )
 
         try:
             page = soup.find("li", class_="next").a.get('href')
         except:
-            return table_urls
+            return product_page_url
         else:
             page = url + page
 
 
 
 
-table_urls = get_all_urls('http://books.toscrape.com/catalogue/')
+product_page_url = get_all_urls('http://books.toscrape.com/catalogue/')
 
-extraire_donnees(table_urls)
+extraire_donnees(product_page_url)
 
 
 
