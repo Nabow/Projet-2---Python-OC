40cbc54a036b34fcf06a2d489b988c2937c1c5a3
Raphael Huet de Froberville <rdefrob@gmail.com>
2021-09-20 15:46:06 +0200

    commit avant test

--- book_to_scrap.py
+++ book_to_scrap.py
@@ -2,7 +2,15 @@ import requests
 from bs4 import BeautifulSoup
 import csv
 import nums_from_string
+from urllib.parse import urlparse
 
+Rating_Table = {
+    "One": 1,
+    "Two": 2,
+    "Three": 3,
+    "Four": 4,
+    "Five": 5
+    }
 
 def get_in_soup(url):
         # lien de la page à scrapper
@@ -25,6 +33,7 @@ def extraire_donnees(elements):
     review_rating = []
     image_url = []
     results = []
+    global url
 
 	for element in elements:
 
@@ -38,16 +47,18 @@ def extraire_donnees(elements):
         availability = soup.find("p" , class_="instock availability").text
         number_available.append(nums_from_string.get_nums(availability))
 
-        product_description.append(soup.find( id="product_description" ).next_sibling.next_sibling)  #Ne marche pas
+        product_description.append(soup.find( id="product_description" ).next_sibling.next_sibling)
 
         test = soup.find("ul" , class_="breadcrumb" )
-        cat = test.select("li:nth-child(3) > a") #Ne marche pas
-        category.append(cat.string)
+        cat = test.findAll("a")[2].text
+        category.append(cat)
 
 
-        review_rating_score = soup.find("p" , class_="star-rating *")
-    
-        image_url
+        rating = soup.find("p" , class_="star-rating").attrs["class"][1]
+        review_rating.append(Rating_Table[rating])
+
+        img = soup.find("div" , class_="item active").img.get_attribute_list("src")
+        image_url.append(img.replace("../..", urlparse(element).netloc))
 
     return results = [elements, universal_product_code, title_product, price_including_tax, price_excluding_tax, number_available, product_description, category, review_rating, image_url ]
 
@@ -87,23 +98,28 @@ def get_all_urls(url):
             page = url + page
 
 
+url='http://books.toscrape.com/catalogue/'
 
+product_page_url = get_all_urls(url)
 
-product_page_url = get_all_urls('http://books.toscrape.com/catalogue/')
+datas = extraire_donnees(product_page_url)
 
-extraire_donnees(product_page_url)
+en_tetes = [ 
+"product_page_url",
+"universal_product_code (upc)",
+"title",
+"price_including_tax",
+"price_excluding_tax",
+"number_available",
+"product_description",
+"category",
+"review_rating",
+"image_url"
+]
 
-
-
-# en_tetes = [ 
-# "product_page_url",
-# "universal_product_code (upc)",
-# "title",
-# "price_including_tax",
-# "price_excluding_tax",
-# "number_available",
-# "product_description",
-# "category",
-# "review_rating",
-# "image_url"
-# ]
+with open(datas.csv, 'w') as fichier_csv:
+    writer = csv.writer(fichier_csv, delimiter=',')
+    writer.writerow(en_tetes)
+    # zip permet d'itérer sur deux listes à la fois
+    for data in datas:
+        writer.writerow(data)
