ac3d6d2fa4a77a434f9f6735bfab685c1437515e
Raphael Huet de Froberville <rdefrob@gmail.com>
2021-09-24 01:31:27 +0200

    Création d'un fichier main

--- book_to_scrap.py
+++ book_to_scrap.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 import csv
 import nums_from_string
 from urllib.parse import urlparse
-# import pandas as pd
+
 
 Rating_Table = {
     "One": 1,
@@ -11,7 +11,7 @@ Rating_Table = {
     "Three": 3,
     "Four": 4,
     "Five": 5
-    }
+}
 
 
 def get_in_soup(url):
@@ -44,31 +44,33 @@ def extraire_donnees(elements):
 
         soup = get_in_soup(element)
 
-        universal_product_code_UPC.append(soup.find("th" , text="UPC" ).next_sibling.text)
+        universal_product_code_UPC.append(soup.find("th", text="UPC").next_sibling.text)
         title_product.append(soup.h1.string)
 
-        price_including_tax.append(nums_from_string.get_nums(soup.find("th" , text="Price (incl. tax)" ).next_sibling.text)[0])
-        price_excluding_tax.append(nums_from_string.get_nums(soup.find("th" , text="Price (excl. tax)" ).next_sibling.text)[0])
-        
-        availability = soup.find("p" , class_="instock availability").text
+        price_including_tax.append(
+            nums_from_string.get_nums(soup.find("th", text="Price (incl. tax)").next_sibling.text)[0])
+        price_excluding_tax.append(
+            nums_from_string.get_nums(soup.find("th", text="Price (excl. tax)").next_sibling.text)[0])
+
+        availability = soup.find("p", class_="instock availability").text
         try:
             number_available.append(nums_from_string.get_nums(availability)[0])
         except:
             number_available.append(0)
 
         try:
-            product_description.append(soup.find( id="product_description" ).next_sibling.next_sibling.text)
+            product_description.append(soup.find(id="product_description").next_sibling.next_sibling.text)
         except:
             product_description.append("")
 
-        test = soup.find("ul" , class_="breadcrumb" )
+        test = soup.find("ul", class_="breadcrumb")
         cat = test.findAll("a")[2].text
         category.append(cat)
 
-        rating = soup.find("p" , class_="star-rating").attrs["class"][1]
+        rating = soup.find("p", class_="star-rating").attrs["class"][1]
         review_rating.append(Rating_Table[rating])
 
-        img = soup.find("div" , class_="item active").img.get_attribute_list("src")
+        img = soup.find("div", class_="item active").img.get_attribute_list("src")
         site_url = urlparse(element).netloc
         img_url = img[0].replace("../..", site_url)
         image_url.append(img_url)
@@ -86,20 +88,20 @@ def charger_donnees(nom_fichier, en_tete, titres, descriptions):
 
 def scroll_page():
     print()
-    
 
-def get_all_urls(url, nb_page = 100000000):
+
+def get_all_urls(url, nb_page=100000000):
     page = url + "page-14.html"
 
     product_page_url = []
-    i=0
-    while (i<nb_page):
-        
+    i = 0
+    while (i < nb_page):
+
         soup = get_in_soup(page)
 
-        products = soup.find_all("h3")  
+        products = soup.find_all("h3")
         for product in products:
-            product_page_url.append( url + product.find("a").get('href') )
+            product_page_url.append(url + product.find("a").get('href'))
 
         try:
             page = soup.find("li", class_="next").a.get('href')
@@ -111,33 +113,29 @@ def get_all_urls(url, nb_page = 100000000):
     return product_page_url
 
 
-url='http://books.toscrape.com/catalogue/'
-
-product_page_url = get_all_urls(url,1)
-
-datas = extraire_donnees(product_page_url)
 
 
 # print(datas)
 
 with open('datas.csv', 'w', newline='', encoding="utf-8") as fichier_csv:
-    en_tetes = [ 
-    "product_page_url",
-    "universal_product_code_UPC (upc)",
-    "title",
-    "price_including_tax",
-    "price_excluding_tax",
-    "number_available",
-    "product_description",
-    "category",
-    "review_rating",
-    "image_url"
+    en_tetes = [
+        "product_page_url",
+        "universal_product_code_UPC (upc)",
+        "title",
+        "price_including_tax",
+        "price_excluding_tax",
+        "number_available",
+        "product_description",
+        "category",
+        "review_rating",
+        "image_url"
     ]
     writer = csv.writer(fichier_csv, delimiter=';')
     writer.writerow(en_tetes)
     for i in range(len(product_page_url)):
-        ligne = [str(product_page_url[i]), universal_product_code_UPC[i], str(title_product[i]), price_including_tax[i], price_excluding_tax[i], number_available[i], str(product_description[i]), category[i], review_rating[i], str(image_url[i])]
+        ligne = [str(product_page_url[i]), universal_product_code_UPC[i], str(title_product[i]), price_including_tax[i],
+                 price_excluding_tax[i], number_available[i], str(product_description[i]), category[i],
+                 review_rating[i], str(image_url[i])]
         # ligne = ligne.text.encode('utf8').decode('ascii', 'ignore')
         # ligne = str(ligne)
         writer.writerow(ligne)
-        
---
+++ main.py
@@ -0,0 +1,6 @@
+import book_to_scrap as bts
+
+if __name__ == '__main__':
+    url = 'http://books.toscrape.com/catalogue/'
+    product_page_url = bts.get_all_urls(url, 1)
+    datas = bts.extraire_donnees(product_page_url)
